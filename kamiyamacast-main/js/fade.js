b553aa2aba1745440c0425abe4821256e263bff6
rimiluu <165989361+rimiluu@users.noreply.github.com>
2026-03-29 19:44:05 +0900

    Delete kamiyamacast-main directory

--- kamiyamacast-main/js/fade.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const fadeInBlock = document.getElementById('fadeInBlock');
-    setTimeout(() => {
-      fadeInBlock.style.opacity = 1;
-      fadeInBlock.style.transform = 'translateY(0)'; // 元の位置に移動
-    }, 100);
-  });
-  
